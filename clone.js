b59b86a4e5cd5501781cf252a83476e44a672389
Bolaji Adedeji <olaola41686@gmail.com>
2023-07-26 04:35:15 +0100

    sidebar and dark mode functionalitties

--- clone.js
+++ clone.js
@@ -15,6 +15,12 @@ const postModalX = document.querySelector('.modal-header i');
 const modalPostBtn = document.querySelector('.modal-header button');
 const modalFooterPlus = document.querySelector('.modal-footer span');
 const modalInput = document.querySelector('.modal-input');
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const xBtn = document.querySelector('.sidebar-header i');
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
 
 
 
@@ -113,8 +119,33 @@ postModalX.addEventListener('click', () => {
 
 });
 
+//sidebar
+user.addEventListener('click', () => {
+    sidebar.classList.add('sidebar-display');
+    sidebarWrapper.classList.add('sidebar-wrapper-display');
 
 
+})
 
+xBtn.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-display');
+    sidebarWrapper.classList.remove('sidebar-wrapper-display');
+});
 
+//dark mode
 
+ const darkElement1 =document.querySelectorAll('.dark-mode-1');
+ const darkElement2 = document.querySelectorAll('.dark-mode-2');
+ const lighTexts = document.querySelectorAll('.light-text');
+ const border = document.querySelectorAll('.border');
+
+ 
+toggle.addEventListener('click', () => {
+    circle.classList.toggle('move');
+    Array.from(darkElement1).map((darkEli1) => darkEli1.classList.toggle('dark-1') )
+    Array.from(darkElement2).map((darkEli2) => darkEli2.classList.toggle('dark-2') )
+    Array.from(lighTexts).map(lighTexts => lighTexts.classList.toggle('light'));
+    Array.from(border).map(border => border.classList.toggle("border-color"));
+  
+
+});
